84be1bc4fb6b9fbedde92dbfb5785d17b5db0b5a
jeonghunkim2021 <jeonghun.kim@monolith.co.kr>
2023-07-21 00:12:51 +0900

    리펙토링

--- lib/ghost_leg.dart
+++ lib/ghost_leg.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 import 'dart:math';
+import 'dart:ui';
 
 // make class ghost_leg
 class Point {
@@ -60,5 +61,85 @@ class GhostLeg {
     legs = legs.toSet().toList();
     legs.sort((a, b) => a.y.compareTo(b.y));
     return legs;
+  }
+
+  void drawPlayerLine(Canvas canvas, List<Point> playerLine, Paint linePaint) {
+    var x = 0.0;
+    var y = 0.0;
+
+    for (var i = 0; i < playerLine.length - 1; i++) {
+      canvas.drawLine(
+        Offset(playerLine[i].x, playerLine[i].y),
+        Offset(playerLine[i + 1].x, playerLine[i + 1].y),
+        linePaint,
+      );
+    }
+  }
+
+  List<Point> makeLegDraw(int currentPlayerNum, int totalPlayerNum,
+      List<Point> legs, double width, double height) {
+    List<Point> result = [];
+
+    Point curPoint = Point(currentPlayerNum * width, 0);
+    result.add(curPoint);
+
+    var lastY = 0.0;
+    //for 반복 legs
+    for (var i = 0; i < legs.length; i += 2) {
+      var x1 = legs[i].x;
+      var y1 = legs[i].y;
+      var x2 = legs[i + 1].x;
+      var y2 = legs[i + 1].y;
+      if (curPoint.x == x1) {
+        curPoint = Point(x1, y1);
+        result.add(curPoint);
+        curPoint = Point(x2, y2);
+        result.add(curPoint);
+      } else if (curPoint.x == x2) {
+        curPoint = Point(x2, y2);
+        result.add(curPoint);
+        curPoint = Point(x1, y1);
+        result.add(curPoint);
+      }
+    }
+
+    curPoint = Point(curPoint.x, height);
+    result.add(curPoint);
+
+    return result;
+  }
+
+  void drawLegs(Canvas canvas, List<Point> legs, var playerNum, double width,
+      double height, Paint lines) {
+    //세로 라인
+    for (var i = 0; i < legs.length; i += 2) {
+      var x1 = 0.0;
+      var x2 = 0.0;
+      if (legs[i].x > legs[i + 1].x) {
+        x1 = legs[i].x;
+        x2 = legs[i + 1].x;
+      } else {
+        x1 = legs[i + 1].x;
+        x2 = legs[i].x;
+      }
+
+      canvas.drawLine(
+        // a점의 (x, y)
+        Offset(x1, legs[i].y),
+        // b점의 (x, y)
+        Offset(x2, legs[i].y),
+        lines,
+      );
+    }
+
+    for (var i = 0; i <= playerNum; i++) {
+      canvas.drawLine(
+        // a점의 (x, y)
+        Offset(i * width, 0),
+        // b점의 (x, y)
+        Offset(i * width, height),
+        lines,
+      );
+    }
   }
 }
